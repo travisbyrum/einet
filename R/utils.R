7f6cf2e019813b35b1b78b02b916be4c78d190d2
Travis Byrum <travis.tbyrum@gmail.com>
2020-01-23 06:58:28 +0000

    Add README.md

--- R/utils.R
+++ R/utils.R
@@ -17,29 +17,3 @@ is_truthy <- function(x) {
     !identical(nchar(x), 0L) &&
     !identical(x, FALSE)
 }
-
-entrospy <- function(x, base = 2) {
-  #"""
-   # pk = asarray(pk)
-   # pk = 1.0*pk / np.sum(pk, axis=axis, keepdims=True)
-  #  if qk is None:
-  #      vec = entr(pk)
- #   else:
-   #     qk = asarray(qk)
-    #    if qk.shape != pk.shape:
-  #          raise ValueError("qk and pk must have same shape.")
-   #     qk = 1.0*qk / np.sum(qk, axis=axis, keepdims=True)
-   #     vec = rel_entr(pk, qk)
-    #S = np.sum(vec, axis=axis)
-    #if base is not None:
-     #   S /= log(base)
-   # return S
-   # """
-
-  #entropy([9/10, 1/10], base=2)
-
-  assertthat::assert_that(is.array(x))
-
-  x = x / sum(x)
-
-}
