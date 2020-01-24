1e31a7dfc08a1040cd36fc2b5fe9784b0df95ada
Travis Byrum <travis.tbyrum@gmail.com>
2020-01-24 00:49:36 -0500

    Add test cases

--- R/effective_information.R
+++ R/effective_information.R
@@ -2,41 +2,42 @@
 effective_information <- function(x, ...) UseMethod("effective_information")
 
 #' @export
-effective_information.matrix <- function(x) {
+effective_information.matrix <- function(x = 0) {
   assertthat::assert_that(is.matrix(x))
 
   graph <- igraph::graph.adjacency(x,  mode = "directed")
-
+  nodes <- igraph::V(graph)
   out_edges <- igraph::incident_edges(graph, igraph::V(graph), mode = "out")
 
-  # TODO CHECK IF HAS WEIGHTS
-
   graph <- graph %>%
+    igraph::set_edge_attr("weight", igraph::E(.), 0) %>%
     igraph::set_edge_attr("weight", out_edges, 1 / length(out_edges))
 
-  number_of_nodes <- igraph::gsize(graph)
+  # TODO CHECK IF HAS WEIGHTS
 
   if (length(out_edges) == 0) {
     return(0)
   }
 
-  w <- lapply(
-    igraph::V(graph),
-    function(x) {
-      weight <- igraph::edge_attr(graph, "weight", x)
+  w_out <- vector(mode = "numeric", length = length(nodes))
+  w_in <- vector(mode = "numeric", length = length(nodes))
 
-      list(
-        w_out = entropy::entropy(weight, unit = "log2"),
-        w_in  = igraph::edge_attr(graph, "weight", x) / length(out_edges)
-      )
-    }
-  )
+  set_win <- function(i) {
+    weight <- igraph::edge_attr(graph, "weight", nodes[i])
+    w_out[i] <<- entropy::entropy(weight, unit = "log2")
 
-  w_out_average <- sapply(w, function(t) t$w_out) %>%
-    sum / length(out_edges)
+    out_edges <- igraph::incident(graph, nodes[i], mode = "out")
+    j <- igraph::ends(graph, out_edges)[2]
 
-  win_entropy <- sapply(w, function(t) t$w_in) %>%
-    entropy::entropy(unit = "log2")
+    w_in[j] <<- w_in[j] +
+      out_edges %>%
+      igraph::edge_attr(graph, "weight", .)
+  }
+
+  lapply(seq_along(nodes), set_win)
+
+  w_out_average <- sum(w_out) / length(out_edges)
+  win_entropy <- entropy::entropy(w_in, unit = "log2")
 
   win_entropy - w_out_average
 }
--- tests/testthat/test_effective_information.R
+++ tests/testthat/test_effective_information.R
@@ -1,7 +1,9 @@
 context("Testing Effective Information")
 
+MAX_ERROR <- 0.001
+
 ### Testing Effective Information ---------------------------------------------
-test_that("is_truthy", {
+test_that("effective_information is correctly calculated", {
   copy_copy <- matrix(
     cbind(
       c(1.0, 0.0, 0.0, 0.0),
@@ -12,5 +14,49 @@ test_that("is_truthy", {
     nrow = 4
   )
 
+  and_and <- matrix(
+    rbind(
+      c(1.0, 0.0, 0.0, 0.0),
+      c(1.0, 0.0, 0.0, 0.0),
+      c(1.0, 0.0, 0.0, 0.0),
+      c(0.0, 0.0, 0.0, 1.0)
+    ),
+    nrow = 4
+  )
+
+  or_or <- matrix(
+    rbind(
+      c(1.0, 0.0, 0.0, 0.0),
+      c(0.0, 0.0, 0.0, 1.0),
+      c(0.0, 0.0, 0.0, 1.0),
+      c(0.0, 0.0, 0.0, 1.0)
+    ),
+    nrow = 4
+  )
+
+  or_copy <- matrix(
+    rbind(
+      c(1.0, 0.0, 0.0, 0.0),
+      c(0.0, 0.0, 1.0, 0.0),
+      c(0.0, 0.0, 0.0, 1.0),
+      c(0.0, 0.0, 0.0, 1.0)
+    ),
+    nrow = 4
+  )
+
+  star <- matrix(
+    rbind(
+      c(1.0, 0.0, 0.0, 0.0),
+      c(1.0, 0.0, 0.0, 0.0),
+      c(1.0, 0.0, 0.0, 0.0),
+      c(1.0, 0.0, 0.0, 0.0)
+    ),
+    nrow = 4
+  )
+
   expect_equal(effective_information(copy_copy), 2.0)
+  expect_lte(effective_information(and_and) - 0.8112781, MAX_ERROR)
+  expect_lte(effective_information(or_or) - 0.8112781, MAX_ERROR)
+  expect_equal(effective_information(or_copy), 1.5)
+  expect_equal(effective_information(star), 0)
 })
